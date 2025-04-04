19daf57c3b3bdc8a0c667c5c1dd867517f6e706f
Giacomo C. <giacomo.corso@gmail.com>
2025-04-04 16:13:08 +0200

    lab: google lab with AR Core and AR Foundation. Pending test illumination

---
+++ Assets/Starter Package/ReticleBehaviour.cs
@@ -0,0 +1,73 @@
+﻿/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using static UnityEngine.XR.ARSubsystems.XRCpuImage;
+
+public class ReticleBehaviour : MonoBehaviour
+{
+    public GameObject Child;
+    public DrivingSurfaceManager DrivingSurfaceManager;
+    public ARPlane CurrentPlane;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Child = transform.GetChild(0).gameObject;
+    }
+
+    private void Update()
+    {
+        // TODO: Conduct a ray cast to position this object.
+        // Determina el centro de la pantalla con el ViewToScreenPoint de la cámara
+        var screenCenter = Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+
+        // La variable hits contendrá ARRaycastHit que describen puntos de los elementos rastreables que se cruzan con ray.
+        var hits = new List<ARRaycastHit>();
+        DrivingSurfaceManager.RaycastManager.Raycast(screenCenter, hits, TrackableType.PlaneWithinBounds);
+
+        // Consulta la lista hits para determinar la intersección de lugares de interés. Prioriza el plano bloqueado contenido en DrivingSurfaceManager y, si no existe, usa el primer hit de plano.
+        CurrentPlane = null;
+        ARRaycastHit? hit = null;
+        if (hits.Count > 0)
+        {
+            // If you don't have a locked plane already...
+            var lockedPlane = DrivingSurfaceManager.LockedPlane;
+            hit = lockedPlane == null
+                // ... use the first hit in `hits`.
+                ? hits[0]
+                // Otherwise use the locked plane, if it's there.
+                : hits.SingleOrDefault(x => x.trackableId == lockedPlane.trackableId);
+        }
+
+        // Si hit contiene un resultado, mueve la transformación de esta GameObject a la posición de hit
+        if (hit.HasValue)
+        {
+            CurrentPlane = DrivingSurfaceManager.PlaneManager.GetPlane(hit.Value.trackableId);
+            // Move this reticle to the location of the hit.
+            transform.position = hit.Value.pose.position;
+        }
+        Child.SetActive(CurrentPlane != null);
+    }
+}
